0ab28e33a90ad07443badf43a6d2c3cb4b840f94
Fernando <fernando.guinda@etec.sp.gov.br>
2022-10-04 22:26:52 -0300

    Adicionado API

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { FilmesService } from '../services/filmes.service';
 
 @Component({
   selector: 'app-home',
@@ -7,10 +8,13 @@ import { Router } from '@angular/router';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  constructor(private router: Router) {
+  constructor(private router: Router, private filmesService: FilmesService) {
 
   }
   SetOpcao1(){
     this.router.navigate(['/opcao1'])
+  }
+  async ngOnInit(){
+    this.filmesService.getProductions();
   }
 }
