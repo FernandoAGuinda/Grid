0967e17852b39d78246ca7ac45a045fe7c78c867
Fernando <fernando.guinda@etec.sp.gov.br>
2022-10-18 22:27:45 -0300

    Consumindo APIs

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
+import { LoadingController } from '@ionic/angular';
 import { FilmesService } from '../services/filmes.service';
 
 @Component({
@@ -8,13 +11,34 @@ import { FilmesService } from '../services/filmes.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  constructor(private router: Router, private filmesService: FilmesService) {
+  filmes:any = [];
+  destaque:any = [];
+  constructor(private alertController: AlertController,
+    public toastController: ToastController,
+    private loadingCtrl: LoadingController,
+    private filmesService: FilmesService,
+    private router: Router) {
 
   }
   SetOpcao1(){
     this.router.navigate(['/opcao1'])
   }
   async ngOnInit(){
+    console.log('Passou no home');
     this.filmesService.getProductions();
+  }
+
+  async carregarFilmes(){
+    this.filmes  = await this.filmesService.getProductions();
+    console.log("filmes carregados", this.filmes)
+
+    const [firstKey] = Object.keys(this.filmes);
+    this.destaque = this.filmes[firstKey];
+
+    this.filmes.splice(firstKey, 1)
+    console.log('firstKey',firstKey)
+    console.log('destaque',this.destaque)
+    console.log('filmes',this.filmes)
+
   }
 }
